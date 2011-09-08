b0e160aa1e5209ab5922f3a3ff7ebfd2e559f3f2
tomasz przechlewski <rudolf.von.ems@gmail.com>
2011-09-08 09:48:39 +0200

    first commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+clean:
+	rm *.dvi p[0-9]*.pdf *.log *.out *.toc *.aux *.ans
---
+++ README.PL
@@ -0,0 +1,9 @@
+Przyk³ady z ksi±¿ki `Praca magisterska i dyplomowa z programem LaTeX'
+
+p21.tex--p46.tex    -- przyk³ady
+wkmgr.cls           -- klasa do sk³adania prac magisterskich/licencjackich
+wkmgr1.tex          -- przyk³ad wykorzystania wkmgr.cls
+wkmgr2.tex          -- inny przyk³ad wykorzystania wkmgr.cls
+
+
+tomasz przechlewski (tprzechlewski@acm.org)
